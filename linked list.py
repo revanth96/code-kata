ff7dc5c8b0ea3530dbe80c0c9238b50f3128a06e
ashwinrishipj <ashwinrishipj@github.com>
2019-09-09 15:08:07 +0530

    linked list

---
+++ linked list.py
@@ -0,0 +1,80 @@
+class Node(object):
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList(object):
+
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next()
+        return count
+
+    def search(self, data):
+        current = self.head
+        found = False
+        while current and found is False:
+            if current.get_data() == data:
+                found = True
+                print ("found")
+            else:
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Data not in list")
+        return current
+
+    def delete(self, data):
+        current = self.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.get_data() == data:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Data not in list")
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+
+obj=LinkedList()
+
+while (1):
+    value =int (input("1.insert \n 2.search \n 3. delete"))
+    if value == 1:
+        data=int (input ("enter the value:"))
+        obj.insert(data)
+    elif value ==2:
+        data=int (input ("enter the value to search:"))
+        obj.search(data)
+    elif value == 3:
+        data=int (input ("enter the value to search:"))
+        obj.delete(data)
+    else:
+        break
